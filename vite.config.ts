3fb5e81356054590be32524635cbe52fd7f4a7a8
Lars-Erik <100536411+Lars-ErikM@users.noreply.github.com>
2026-01-28 09:53:05 +0100

    Backslash typo corrected

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-lakmusTMP2025/", //YOUR REPO NAME HERE
+ //base: "/vite-lakmusTMP2025", //YOUR REPO NAME HERE - Med denne forsvinner logoen (antar hjem ikke lenger er siden som vises)
 })
